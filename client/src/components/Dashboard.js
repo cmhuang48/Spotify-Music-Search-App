0d554c89f3665b0e47b81b696949ab789688debb
Chanelle Huang <chanellehuang@gmail.com>
2022-05-19 15:09:38 -1000

    changed redirect urls for heroku

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -81,7 +81,7 @@ const Dashboard = (props) => {
   useEffect(() => {
     if (!playingTrack) return;
     
-    axios.get('http://localhost:3001/lyrics', { 
+    axios.get('https://spotify-translate.herokuapp.com/lyrics', { 
       params: {
         track: playingTrack.name,
         artist: playingTrack.artists[0].name 
